de5b60e6c1bdd0c7f3cf5d1bc5f8bab3eb347d08
ergrassa <ergrassa@gmail.com>
2022-01-18 23:29:30 +0300

    file changes

--- git-changes.sh
+++ git-changes.sh
@@ -2,4 +2,4 @@
 commit_id=$(git rev-parse --short=11 HEAD)
 diff=$(git diff $commit_id --no-commit-id --name-only)
 branch=$(git rev-parse --abbrev-ref HEAD)
-printf "#Branch\n"$branch"\n#Commit ID\n"$commit_id"\n#Changed files\n"$diff"\n\n---" > README.md
+printf "# Branch\n"$branch"\n# Commit ID\n"$commit_id"\n# Changed files\n"$diff"\n\n---" > README.md
